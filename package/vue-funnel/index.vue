958804f77709e42b7da5955307b378484e68704d
nemoisme <nemo_ambitioner@163.com>
2019-07-16 18:04:33 +0800

    完成漏斗图初始版本功能

--- package/vue-funnel/index.vue
+++ package/vue-funnel/index.vue
@@ -2,18 +2,37 @@
 export default {
   name: 'vue-funnel',
   props: {
-    maxWidth: [String, Number],
-    minWidth: [String, Number],
-    rowSpace: [String, Number]
+    maxWidth: {
+      type: Number,
+      default: 300
+    },
+    minWidth: {
+      type: Number,
+      default: 150
+    },
+    rowSpace: {
+      type: Number,
+      default: 8
+    },
+    rowHeight: {
+      type: Number,
+      default: 50
+    },
+    colors: {
+      type: Array,
+      default: () => ['#576DE7', '#8B74F2', '#A37EEF', '#ccc', '#ccc']
+    },
+    isLeft: {
+      type: Boolean,
+      default: true
+    }
   },
   data() {
     return {}
   },
   computed: {
     dynamicStyle() {
-      return (n, detal, {min, max}) => {
-        
-      }
+      return (n, detal, {min, max}) => {}
     },
     slotsLength() {
       const slots = this.$slots.default.filter(node => !!node.tag)
@@ -26,8 +45,9 @@ export default {
     }
   },
   render(h) {
+    // 过滤掉 slots中 tag为underfined 的 vnode
     const slots = this.$slots.default.filter(node => !!node.tag)
-    const {dynamicStyle, increment} = this
+    console.log(this.$slots, 'this.slots')
     return h(
       'div',
       {
@@ -37,25 +57,81 @@ export default {
       },
       slots.map((slot, index) =>
         h(
-          slot.tag,
+          'div',
           {
-            style: dynamicStyle(index + 1, increment, {
-              max: this.maxWidth,
-              min: this.minWidth
-            })
+            style: {
+              display: 'flex',
+              'min-width': 'auto',
+              [this.isLeft ? 'paddingLeft' : 'paddingRight']:
+                index * this.increment + 'px',
+              marginTop: this.rowSpace + 'px'
+            }
           },
-          slot.children
+          this.renderSlots(h, {slot, index})
         )
       )
     )
+  },
+  methods: {
+    renderSlots(h, {slot, index}) {
+      const {
+        dynamicStyle,
+        increment,
+        renderTriangle,
+        rowHeight,
+        maxWidth
+      } = this
+      const centerVnode = h(
+        slot.tag,
+        {
+          style: {
+            width: maxWidth - index * increment * 2 + 'px',
+            background: this.colors[index],
+            height: rowHeight + 'px',
+            position: 'relative',
+            float: [this.isLeft ? 'left' : 'right']
+          }
+        },
+        slot.children
+      )
+      const infoSlot = h(
+        'div',
+        {
+          style: {
+            [this.isLeft ? 'paddingLeft' : 'paddingRight']:
+              index * this.increment + 'px',
+            flex: 1
+          }
+        },
+        this.$slots[`info${index}`]
+      )
+      const vnode = [
+        renderTriangle(h, index, 'left'),
+        centerVnode,
+        renderTriangle(h, index, 'right')
+      ]
+      vnode.splice(this.isLeft ? vnode.length : 0, 0, infoSlot)
+      return vnode
+    },
+    renderTriangle(h, index, type = 'left') {
+      const {rowHeight, increment} = this
+      return h(
+        'div',
+        {
+          style: {
+            position: 'relative',
+            float: [this.isLeft ? 'left' : 'right'],
+            width: 0,
+            height: 0,
+            borderTop:
+              rowHeight + 'px' + ' ' + 'solid' + ' ' + this.colors[index],
+            [type == 'left' ? 'borderLeft' : 'borderRight']:
+              increment + 'px' + ' ' + 'solid' + ' ' + 'transparent'
+          }
+        },
+        this.$slots[`arrow-${type}${index}`]
+      )
+    }
   }
 }
 </script>
-<style lang="less">
-.vue-funnel {
-  .funel-box {
-    width: 100%;
-    display: inline-block;
-  }
-}
-</style>
